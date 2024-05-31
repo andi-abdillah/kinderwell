aab8963cd9b052e35689315358ca02d64e176960
Amin Abdillah <andiabdillah255@gmail.com>
2024-05-31 08:29:48 +0700

    add animation on feelings page

--- src/pages/feelings/FeelingsItems.jsx
+++ src/pages/feelings/FeelingsItems.jsx
@@ -5,22 +5,22 @@ import Icon from "../../components/Icon";
 export default function FeelingsItems() {
   return (
     <>
-      <div className="flex flex-wrap justify-center gap-12 mt-16 pb-24">
+      <div className="flex flex-wrap justify-center md:gap-12 mt-36 md:mt-16 pb-24">
         {FeelingsData.map((item, index) => (
           <div
             key={index}
             style={{ backgroundColor: item.color }}
-            className="flex gap-4 w-[27rem] h-64 p-6 rounded-3xl shadow-health_card"
+            className={`flex flex-col md:flex-row gap-4 -mt-16 md:mt-auto w-[27rem] h-content p-6 rounded-3xl shadow-health_card transition duration-500 ease-in-out hover:z-50 hover:-translate-y-6`}
           >
             <img
-              className="h-52 w-40 drop-shadow-icon"
+              className="w-32 md:w-40 md:h-52 m-auto drop-shadow-icon"
               src={`./${item.image}`}
               alt={item.image}
             />
             <div className="flex flex-col items-center h-full text-white text-center">
-              <h2 className="text-3xl font-bold">{item.title}</h2>
-              <p className="text-lg font-semibold my-auto">{item.caption}</p>
-              <div className="flex items-center justify-between w-full rounded-full">
+              <h2 className="text-lg md:text-3xl font-bold">{item.title}</h2>
+              <p className="md:text-lg font-semibold my-auto">{item.caption}</p>
+              <div className="flex mt-5 items-center justify-between w-full rounded-full">
                 <Icon className="text-3xl font-black">more_horiz</Icon>
                 <Link
                   to={`${item.slug}`}
